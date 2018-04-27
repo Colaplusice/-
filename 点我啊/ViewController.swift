310b5d5b4094e3f66bae8f2f53ce3c0c72459afe
Colaplusice <fjl2401@163.com>
2018-04-27 20:04:07 +0800

    Initial Commit

---
+++ 点我啊/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  点我啊
+//
+//  Created by 樊佳亮 on 2018/4/27.
+//  Copyright © 2018年 樊佳亮. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
